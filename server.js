add7b9ae8281711df0ce4f84043557618c748a15
MediMate Developer <developer@medimate.app>
2026-03-23 17:58:03 +0800

    fix: 降级 Express 到 4.x 解决路由兼容性问题

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use('/api', createProxyMiddleware({
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // SPA fallback
-app.get('(.*)', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
